a0b94d59d846a355e4950909140cec59b372bfa9
Santi <Santi@DESKTOP-4MSMQ7M>
2020-05-13 21:37:50 -0300

    Initial commit

---
+++ app/src/main/java/com/example/queesunactiviti/segundoactiviti.java
@@ -0,0 +1,14 @@
+package com.example.queesunactiviti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class segundoactiviti extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundoactiviti);
+    }
+}
